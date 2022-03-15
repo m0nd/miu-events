9783ae9010f5456e56ac71cb863aa64022465435
Berhanu G. Desta <bertseg@gmail.com>
2022-03-15 02:04:15 -0500

    added token verification middleware for protected routes

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -1,18 +1,21 @@
 const express = require("express");
+const verifyToken = require("../middlewares/verifyToken");
 const {
   createEvent,
   getAllEvents,
   getRecentEvents,
   updateEvent,
   deleteEvent,
+  filterEvents,
 } = require("../controllers/eventController");
 
 const router = express.Router();
 
 router.get("/", getAllEvents);
 router.get("/recent/:numberOfEvents", getRecentEvents);
-router.post("/", createEvent);
-router.patch("/:eventId", updateEvent);
-router.delete("/:eventId", deleteEvent);
+router.get("/filter", filterEvents);
+router.post("/", verifyToken, createEvent);
+router.patch("/:eventId", verifyToken, updateEvent);
+router.delete("/:eventId", verifyToken, deleteEvent);
 
 module.exports = router;
